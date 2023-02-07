f11d5a84cb34f7db1891a255cd47aed2f349d12b
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-02-07 18:00:01 +0100

    :sparkles: Created page template

---
+++ src/components/pages/Root.tsx
@@ -0,0 +1,13 @@
+import PageTemplate from '../templates/MainTemplate';
+
+const Root = () => {
+    return (
+        <PageTemplate>
+            <div>
+                <p>children content</p>
+            </div>
+        </PageTemplate>
+    );
+};
+
+export default Root;
